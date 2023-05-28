a2ff844452be3d11836c87c81032c95e4349f03f
PSKP-95 <patil8698parikshit@gmail.com>
2023-05-28 22:59:34 +0530

    hello world

---
+++ 00-hello-world/00_hello_world.rs
@@ -0,0 +1,12 @@
+
+// This is single line comment
+/* This is multiline comment.
+ * you can span this on multiline
+ */
+fn main() {
+  println!("Hello World");
+
+  // formatted hello, <xyz>
+  let greet = format!("Hello, {}, How is {}?", "ABC", "PQR");
+  println!("{}", greet);
+}
